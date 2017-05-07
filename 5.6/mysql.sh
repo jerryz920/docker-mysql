66c29bab8392a392d9412a536dd5354c9d375884
Yan Zhai <yanzhai@cs.wisc.edu>
2017-05-06 23:13:00 -0500

    socc17 scripts

--- 5.6/mysql.sh
+++ 5.6/mysql.sh
@@ -1,1 +1,2 @@
- docker run --rm -i -t -p 13306:3306 -e MYSQL_ROOT_PASSWORD= -e MYSQL_DATABASE=wikidb -e MYSQL_USER=wikiuser -e MYSQL_PASSWORD=wikipass mysql
+ docker run --rm -i -t -p 3307:3307 -p 13306:3306 -e MYSQL_ROOT_PASSWORD=root@sonar -e MYSQL_DATABASE=wikidb -e MYSQL_USER=wikiuser -e MYSQL_PASSWORD=wikipass mysql docker-entrypoint.sh mysqld
+
--- 5.6/verify.sh
+++ 5.6/verify.sh
@@ -1,8 +1,10 @@
 
-b1=$1
-b2=$((b1-1))
-b3=$((b2-1))
-curl http://compute5:7777/appAccessesObject -d "{ \"principal\": \"152.3.145.138:4144\",  \"otherValues\": [\"1.1.1.1:$b1\", \"alice:object1\"]}"
-curl http://compute5:7777/appAccessesObject -d "{ \"principal\": \"152.3.145.138:4144\",  \"otherValues\": [\"1.1.1.1:$b2\", \"alice:object1\"]}"
-curl http://compute5:7777/appAccessesObject -d "{ \"principal\": \"152.3.145.138:4144\",  \"otherValues\": [\"1.1.1.1:$b3\", \"alice:object1\"]}"
 
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
+curl -q -s http://compute4-elink:8098/types/safesets/buckets/safe/keys/w07PpxOmjvF3PfmexNk9H0vzJwHBlzMUYjt1DyO8fQ8
